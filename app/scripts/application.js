0273a4858d2fad4c71d881ca411ed61b4038d96c
Hrvoje Šimić <shime.ferovac@gmail.com>
2014-08-03 14:32:00 +0200

    add some speech

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -3,6 +3,15 @@ window.MathUtils.randomInt = function(min, max){
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+window.Speech = {};
+window.Speech.say = function(what){
+  var fallbackSpeechSynthesis = window.getSpeechSynthesis(),
+      fallbackSpeechSynthesisUtterance = window.getSpeechSynthesisUtterance(),
+      u = new fallbackSpeechSynthesisUtterance(what);
+
+  fallbackSpeechSynthesis.speak(u);
+}
+
 log = function() { return console.log.apply(console, arguments); };
 
 window.App = Ember.Application.create();
