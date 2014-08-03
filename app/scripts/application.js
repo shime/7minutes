1f20f7a331e26f324ef3233ed455ab2c10d71db3
Hrvoje Šimić <shime.ferovac@gmail.com>
2014-08-03 19:49:32 +0200

    add state machine

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -15,7 +15,42 @@ log = function() { return console.log.apply(console, arguments); };
 
 $(document).ready(function(){
   $('img').attr('src', 'images/' + Utils.randomInt(1, 4) + '.jpg');
+  $('body').on('keypress', function(){
+    window.MainState.startTraining();
+  });
 });
+
+
+function MainState(){
+  var machine = new MicroMachine("initializing");
+  machine.transitionsFor.training = {initializing: 'training'};
+  machine.transitionsFor.finished = {training: 'finished'};
+
+  var self = this;
+  machine.on("any", function(machine){
+    self.state = machine.state;
+  });
+
+  this.startTraining = function(){
+    $('body').off('keypress');
+    machine.trigger('training');
+    this.changeStep(window.STEPS[0]);
+  };
+
+  this.endTraining = function(){
+    machine.trigger('finished');
+  };
+
+  this.changeStep = function(step){
+    self.step = step;
+    Utils.say(step.content);
+    $('#content').text(step.content);
+  };
+
+  this.state = undefined;
+
+  return this;
+};
 
 var rest = {id: 'break', content: 'take a break'};
 var step = function(which){
@@ -51,3 +86,5 @@ window.STEPS = [
   step('left side plank'),
   step('right side plank')
 ];
+
+window.MainState = new MainState();
