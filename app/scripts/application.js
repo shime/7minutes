7ce647efd8958d93804d376505c8b740607b1e18
Hrvoje Šimić <shime.ferovac@gmail.com>
2014-08-03 15:39:31 +0200

    add all exercises

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -6,13 +6,66 @@ window.MathUtils.randomInt = function(min, max){
 window.Speech = {};
 window.Speech.say = function(what){
   var fallbackSpeechSynthesis = window.getSpeechSynthesis(),
-      fallbackSpeechSynthesisUtterance = window.getSpeechSynthesisUtterance(),
-      u = new fallbackSpeechSynthesisUtterance(what);
+      FallbackSpeechSynthesisUtterance = window.getSpeechSynthesisUtterance(),
+      u = new FallbackSpeechSynthesisUtterance(what);
 
   fallbackSpeechSynthesis.speak(u);
-}
+};
 
 log = function() { return console.log.apply(console, arguments); };
 
+var defaults = {
+  'img': 'images/' + MathUtils.randomInt(1, 4) + '.jpg',
+  'content': 'workout ready, press anything'
+};
+
 window.App = Ember.Application.create();
-App.set('img', 'images/' + MathUtils.randomInt(1, 4) + '.jpg');
+App.setProperties(defaults);
+
+App.reopen({
+  speaker: function(){
+    Speech.say(this.get('content'));
+  }.observes('content')
+});
+
+var attr = Em.attr;
+App.Step = Em.Model.extend({
+  content: attr()
+});
+
+App.Step.adapter = Em.FixtureAdapter.create();
+
+var rest = {id: 'break', content: 'take a break'};
+var step = function(which){
+  return {
+    id: which.replace(/ /g, '-'),
+    content: which
+  };
+};
+
+App.Step.FIXTURES = [
+  step('jumping jacks'),
+  rest,
+  step('wall sit'),
+  rest,
+  step('push-up'),
+  rest,
+  step('abdominal crunch'),
+  rest,
+  step('step-up onto chair'),
+  rest,
+  step('squat'),
+  rest,
+  step('triceps dip onto chair'),
+  rest,
+  step('plank'),
+  rest,
+  step('high knees running in place'),
+  rest,
+  step('lunge'),
+  rest,
+  step('push-up and rotation'),
+  rest,
+  step('left side plank'),
+  step('right side plank')
+];
--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,1 +1,12 @@
-App.Router.map(function() { });
+App.Router.map(function() {
+  this.route('step', { path: 'step/:id' });
+});
+
+App.StepRoute = Em.Route.extend({
+  model: function(params){
+    return App.Step.fetch(params.id);
+  },
+  setupController: function(controller, model){
+    App.set('content', model.get('content'));
+  }
+});
