045a3f3d63963bf75039c44991b5eaf08153f2c2
Hrvoje Šimić <shime.ferovac@gmail.com>
2014-08-03 13:24:14 +0200

    add some random pics

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -1,1 +1,9 @@
+window.MathUtils = {};
+window.MathUtils.randomInt = function(min, max){
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+log = function() { return console.log.apply(console, arguments); };
+
 window.App = Ember.Application.create();
+App.set('img', 'images/' + MathUtils.randomInt(1, 4) + '.jpg');
